16ee49895463dfe11a65789310468aed450d33c0
ongaro09 <ongaromark7@gmail.com>
2024-08-16 05:25:43 +0300

    updated

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python3
+"""A script to rotate a 2D matrix"""
+
+
 def rotate_2d_matrix(matrix):
     n = len(matrix)
-    
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
-    # Reverse each row
-    for i in range(n):
-        matrix[i].reverse()
+
+    for layer in range(n // 2):
+        first = layer
+        last = n - 1 - layer
+        for i in range(first, last):
+            top = matrix[first][row]
+            matrix[first][row] = matrix[last - row + first][first]
+            matrix[last - row + first][first] = matrix[last][last - row + first]
+            matrix[last][last - row + first] = matrix[row][last]
+            matrix[row][last] = top
